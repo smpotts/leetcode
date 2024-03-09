74cc2f02cbfb5e2f1364e68d008214f9d576583f
Shelby Potts <shelbypotts.connect@gmail.com>
2024-03-09 08:51:23 -0700

    clean up

--- src/birthday_paradox.py
+++ src/birthday_paradox.py
@@ -5,10 +5,10 @@ en.wikipedia.org/wiki/Birthday_paradox
 Hint: you can generate random birthdays with the randint function in the random module.
 """
 import random
-from math import prod
 
 
 def has_duplicates(lst):
+    # return T or F if there are duplicates in the list
     return len(lst) != len(set(lst))
 
 
@@ -18,10 +18,7 @@ def birthday_probability(n_simulations=1000, n_students=23):
     for _ in range(n_simulations):
         students = [random.randint(1, 365) for _ in range(n_students)]
 
-        # how to determine the probability there are two students with the same birthday?
-        # can use the concept of complementary probability, which states that the probability of an event occurring is
-        # 1 minus the probability of it not occurring
-        # P(at least one repeats) = 1 - P(no repeats)
+        # determine the probability there are two students with the same birthday as a proportion
         if has_duplicates(students):
             count_duplicates += 1
     return count_duplicates / n_simulations
